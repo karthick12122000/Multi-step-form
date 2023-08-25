e39cac6fa317650e339b806d551cd62625820845
karthick12122000 <karthicknammakkal@gmail.com>
2023-08-25 21:58:28 +0530

    Development in active class in js

--- Index.js
+++ Index.js
@@ -1,1 +1,19 @@
 
+function activeFun(e) {
+    var activEle = document.querySelector(".active");
+    console.log(activEle.id);
+
+    var IdList = ["personalInfo", "plan"];
+    console.log(IdList.indexOf(activEle.id));
+    var index = IdList.indexOf(activEle.id);
+    var nID = IdList.at(index + 1);
+    if (nID != null) {
+        activEle.classList.remove("active");
+        console.log(nID);
+        var ele = document.getElementById(nID);
+        console.log(ele);
+        ele.classList.add("active");
+    }
+}
+var nextBTN = document.getElementById("NStep");
+nextBTN.addEventListener('click', activeFun);
