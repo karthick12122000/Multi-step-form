a2df43b166d71b5dfc8044e117b77d0fb89fb0df
karthick12122000 <karthicknammakkal@gmail.com>
2023-08-26 21:50:34 +0530

    Worked on Active class and button

--- Index.js
+++ Index.js
@@ -1,19 +1,48 @@
-
+var GBack = document.getElementById("GBack");
+var nextBTN = document.getElementById("NStep");
 function activeFun(e) {
     var activEle = document.querySelector(".active");
-    console.log(activEle.id);
+    var activeTab = document.querySelector(".activeT")
+
 
     var IdList = ["personalInfo", "plan"];
-    console.log(IdList.indexOf(activEle.id));
+    // console.log(IdList.indexOf(activEle.id));
     var index = IdList.indexOf(activEle.id);
-    var nID = IdList.at(index + 1);
+
+
+
+    if (e == "next") {
+        var nID = IdList[index + 1];
+
+        GBack.style.display = 'initial';
+    }
+    else {
+
+        var nID = IdList[index - 1];
+
+        if ((index - 1) == 0) {
+            GBack.style.display = 'none';
+        }
+        else {
+            GBack.style.display = 'initial';
+        }
+
+    }
     if (nID != null) {
         activEle.classList.remove("active");
-        console.log(nID);
+        activeTab.classList.remove("activeT");
+
         var ele = document.getElementById(nID);
-        console.log(ele);
+
         ele.classList.add("active");
+        var tab = document.getElementById(nID + "T");
+        tab.classList.add("activeT");
+
     }
+
+
 }
-var nextBTN = document.getElementById("NStep");
-nextBTN.addEventListener('click', activeFun);
+
+nextBTN.addEventListener('click', () => { activeFun("next") });
+
+GBack.addEventListener('click', () => { activeFun("back") });
