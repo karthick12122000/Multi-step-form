f892f7d28c97468384d82cddbb0f1c51562b98b5
karthick12122000 <karthicknammakkal@gmail.com>
2023-08-29 20:57:43 +0530

    Development in plan form

--- Index.js
+++ Index.js
@@ -1,3 +1,4 @@
+///////////////------------------------ Go back & Next step button
 var GBack = document.getElementById("GBack");
 var nextBTN = document.getElementById("NStep");
 function activeFun(e) {
@@ -44,5 +45,38 @@ function activeFun(e) {
 }
 
 nextBTN.addEventListener('click', () => { activeFun("next") });
-
 GBack.addEventListener('click', () => { activeFun("back") });
+
+//////////////////////-------------------Plan checkbox
+
+var toggle = document.getElementById("toggle");
+toggle.addEventListener('change',(e)=>{
+let plan=document.querySelector(".plan--switch");
+let planDetails=document.querySelectorAll(".rate");
+let planDiv=document.querySelectorAll(".plan");
+console.log(planDiv);
+
+
+if(toggle.checked==true){
+    plan.childNodes[1].style.color="var(--Coolgray)";
+    plan.childNodes[9].style.color="var(--Marineblue)";
+    planDetails[0].innerText="$90/mo";
+    planDetails[1].innerText="$120/mo";
+    planDetails[2].innerText="$150/mo";
+    planDiv[0].innerHTML+="<p class='free' style='color:var(--Marineblue)'>2 months free</p>";
+    planDiv[1].innerHTML+="<p class='free'  style='color:var(--Marineblue)'>2 months free</p>";
+    planDiv[2].innerHTML+="<p class='free' style='color:var(--Marineblue)'>2 months free</p>";
+
+}
+else{
+    plan.childNodes[9].style.color="var(--Coolgray)";
+    plan.childNodes[1].style.color="var(--Marineblue)";
+    planDetails[0].innerText="$9/mo";
+    planDetails[1].innerText="$12/mo";
+    planDetails[2].innerText="$15/mo";
+    planDiv[0].removeChild(planDiv[0].lastElementChild);
+    planDiv[1].removeChild(planDiv[1].lastElementChild);
+    planDiv[2].removeChild(planDiv[2].lastElementChild);
+}
+
+});
