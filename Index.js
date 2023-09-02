34f285a0d273310916f42eb81e0f64fd860fd797
karthick12122000 <karthicknammakkal@gmail.com>
2023-09-02 21:41:49 +0530

    Development in addon page

--- Index.js
+++ Index.js
@@ -10,8 +10,6 @@ function activeFun(e) {
     // console.log(IdList.indexOf(activEle.id));
     var index = IdList.indexOf(activEle.id);
 
-
-
     if (e == "next") {
         var nID = IdList[index + 1];
 
@@ -54,20 +52,27 @@ toggle.addEventListener('change',(e)=>{
 let plan=document.querySelector(".plan--switch");
 let planDetails=document.querySelectorAll(".rate");
 let planDiv=document.querySelectorAll(".plan");
-console.log(planDiv);
+let addonDetails=document.querySelectorAll(".rateA"); 
 
 
+/////////---------------yearly
 if(toggle.checked==true){
     plan.childNodes[1].style.color="var(--Coolgray)";
     plan.childNodes[9].style.color="var(--Marineblue)";
-    planDetails[0].innerText="$90/mo";
-    planDetails[1].innerText="$120/mo";
-    planDetails[2].innerText="$150/mo";
+    planDetails[0].innerText="$90/yr";
+    planDetails[1].innerText="$120/yr";
+    planDetails[2].innerText="$150/yr";
     planDiv[0].innerHTML+="<p class='free' style='color:var(--Marineblue)'>2 months free</p>";
     planDiv[1].innerHTML+="<p class='free'  style='color:var(--Marineblue)'>2 months free</p>";
     planDiv[2].innerHTML+="<p class='free' style='color:var(--Marineblue)'>2 months free</p>";
 
+
+    addonDetails[0].innerText="+$10/yr";
+    addonDetails[1].innerText="+$20/yr";
+    addonDetails[2].innerText="+$20/yr";
+
 }
+////////--------monthly
 else{
     plan.childNodes[9].style.color="var(--Coolgray)";
     plan.childNodes[1].style.color="var(--Marineblue)";
@@ -77,6 +82,11 @@ else{
     planDiv[0].removeChild(planDiv[0].lastElementChild);
     planDiv[1].removeChild(planDiv[1].lastElementChild);
     planDiv[2].removeChild(planDiv[2].lastElementChild);
+
+
+    addonDetails[0].innerText="+$1/mo";
+    addonDetails[1].innerText="+$2/mo";
+    addonDetails[2].innerText="+$2/mo";
 }
 
 });
